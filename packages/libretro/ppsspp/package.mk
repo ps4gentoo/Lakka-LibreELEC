c9013d15487f3e840fb4409dafe3b203e7f031b8
mircoho <39863436+ps4gentoo@users.noreply.github.com>
2020-01-22 16:32:15 +0100

    Update package.mk

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="e5f1598"
+PKG_VERSION="bf1777f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
